7b54485facb05896f82a095183aba16f89ca1303
GayanPradeep97 <gayanpradeep0212@gmail.com>
2023-03-10 02:42:29 -0800

    update

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup,FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-dialog',
@@ -8,5 +10,25 @@ import { Component } from '@angular/core';
 export class DialogComponent {
 
   fressnessList =["Brand New", "Second Hand", "Refublished"];
+  productForm !: FormGroup;
+  constructor(private formBuilder: FormBuilder){
+
+  }
+
+    ngOnInit(): void{
+      this.productForm =this.formBuilder.group({
+        productname : ['',Validators.required],
+        category :['', Validators.required],
+        date :['', Validators.required],
+        freshness : ['', Validators.required],
+        price : ['', Validators.required],
+        comment : ['', Validators.required],
+
+      })
+      
+    }
+    addProduct(){
+      console.log(this.productForm.value);
+    }
 
 }
